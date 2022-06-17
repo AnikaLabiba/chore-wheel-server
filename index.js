fc8e4e92cc4d92c1394f800eff32ef585c5c8f3c
AnikaLabiba <anikalabibajahin@gmail.com>
2022-06-17 18:01:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000
+
+//middlewares
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2486a.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect()
+        const projectCollection = client.db('projectSynergy').collection('projects')
+
+        app.get('/projects', async (req, res) => {
+            const query = {}
+            const cursor = projectCollection.find(query)
+            const projects = await cursor.toArray()
+            res.send(projects)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
+
+//root api
+app.get('/', async (req, res) => {
+    res.send('My Project Synergy is running.')
+})
+app.listen(port, async (req, res) => {
+    console.log('Listening to port:', port)
+})
