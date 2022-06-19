0f7a08a2e4760b972b8fbb0da237e357fc97f519
AnikaLabiba <anikalabibajahin@gmail.com>
2022-06-19 13:07:20 +0600

    api updated

--- index.js
+++ index.js
@@ -85,8 +85,15 @@ async function run() {
             };
             const result = await projectCollection.updateOne(filter, updatedDoc, options);
             res.send(result);
-
         })
+        //delete project
+        app.delete('/project/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: ObjectId(id) }
+            const result = await projectCollection.deleteOne(filter)
+            res.send(result)
+        })
+
     }
     finally {
 
